dfc17b26a9dae78caebc2cfc50fc2f7d98c106c9
Terrell Hudson <thudson5731@gmail.com>
2021-12-29 16:54:57 -0500

    getting data and storing it

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const time = document.querySelector('.selector');
 const listDay = document.querySelector('.day1');
 
 button.addEventListener('click', addToChart);
-
+let i = 0;
 function addToChart() {
+	let count = ++i;
+	let thisDay = new Date();
 	let writeDate = date.value;
 	let writeTop = topNum.value;
 	let writeBottom = bottomNum.value;
-	listDay.innerHTML += `<li><p><b>Blood Pressure:</b></p> <span class='read'>${writeTop}/  ${writeBottom}</span> <span class="time">${new Date()}</span><hr></li>`;
-	console.log(writeTop);
-	console.log(writeBottom);
-	console.log(writeDate);
+	listDay.innerHTML += `<li><p><b>Blood Pressure:</b></p> <span class='read'>${writeTop}/  ${writeBottom}</span> <span class="time">${thisDay}</span><hr></li>`;
+	localStorage.setItem(`${count}`, `${thisDay},${writeTop}, ${writeBottom}`);
 }
 
 function dateTime() {
